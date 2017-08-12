e052d1438f6a8ffc3c328b63869c6b45fd9462ec
eluts <eluts15@gmai.com>
2017-08-12 16:42:06 -0700

    add bootstrap, add ViewModels for Registering and logging in.

---
+++ src/SonOfCod2/ViewModels/LoginViewModel.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace SonOfCod2.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ src/SonOfCod2/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SonOfCod2.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmation")]
+        [Compare("Password", ErrorMessage = "Passwords must be a match.")]
+        public string Confirm { get; set; }
+    }
+}
