ff0f821c4c0e03429737d6626d3c36705292369f
eluts <eluts15@gmai.com>
2017-08-13 15:31:23 -0700

    add a test to verify email for subscriber works correctly.

---
+++ SonOfCod.Tests/SonOfCod.Tests/ModelTests/SubscriberTest.cs
@@ -0,0 +1,18 @@
+﻿using SonOfCod2.Models;
+using Xunit;
+
+namespace SonOfCod.Tests.ModelTests
+{
+    public class SubscriberTest
+    {
+        [Fact]
+        public void Get_Email_Test()
+        {
+            Subscriber subscriber = new Subscriber("eluts15@gmail.com");
+
+            string result = subscriber.Email;
+
+            Assert.Equal("eluts15@gmail.com", result);
+        }
+    }
+}
