b3305bc2d4cea1e0971e0028eb674ac5289f1207
eluts <eluts15@gmai.com>
2017-08-13 15:44:48 -0700

    add another controller test to very Newsletter View generates correctly.

---
+++ SonOfCod.Tests/SonOfCod.Tests/ControllerTests/NewsletterControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SonOfCod2.Controllers;
+using SonOfCod2.Models;
+using Xunit;
+
+namespace SonOfCod.Tests.ControllerTests
+{
+    public class NewsletterControllerTest
+    {
+        private readonly ApplicationDbContext _db;
+
+        [Fact]
+        public void Get_ViewResult_Index()
+        {
+            NewsletterController controller = new NewsletterController(_db);
+            IActionResult result = controller.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
