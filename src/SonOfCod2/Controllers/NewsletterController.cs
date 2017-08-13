f4a9ec4b4ba3c96037d9259183a966c17f8b13d2
eluts <eluts15@gmai.com>
2017-08-12 17:12:44 -0700

    add Newsletter controller and associated Views to subscribe to newsletter.

---
+++ src/SonOfCod2/Controllers/NewsletterController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SonOfCod2.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace SonOfCod2.Controllers
+{
+    public class NewsletterController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public NewsletterController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult SignUp(Subscriber model)
+        {
+            _db.Subscribers.Add(model);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        [Authorize]
+        public IActionResult Subscribers()
+        {
+            List<Subscriber> subscribers = _db.Subscribers.ToList();
+            return View(subscribers);
+        }
+    }
+}
